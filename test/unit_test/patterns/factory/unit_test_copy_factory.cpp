a5bc1d30677736dab5cc51bc1ee30f202dd22ec4
Thomas R. Benson <benson31@llnl.gov>
2020-01-27 10:29:34 -0800

    add tests for factories and switch dispatcher

---
+++ test/unit_test/patterns/factory/unit_test_copy_factory.cpp
@@ -0,0 +1,134 @@
+// @H2_LICENSE_TEXT@
+
+#include <catch2/catch.hpp>
+
+#include <h2/patterns/factory/CopyFactory.hpp>
+
+#include <memory>
+#include <typeinfo>
+
+namespace h2
+{
+template <typename T>
+std::unique_ptr<T> ToUnique(T* ptr) { return std::unique_ptr<T>(ptr); }
+}
+
+namespace
+{
+struct WidgetBase {
+    virtual int Data() const noexcept = 0;
+    virtual ~WidgetBase() = default;
+};
+
+struct Widget : WidgetBase
+{
+    Widget() : data_(-1) {}
+    Widget(int d) : data_(d) {}
+    Widget* Copy() const { return new Widget(*this); }
+
+    int Data() const noexcept override { return data_; }
+    int data_;
+};
+
+struct Gizmo : WidgetBase
+{
+    Gizmo() : data_(-1.f) {}
+    Gizmo(int d) : data_(d) {}
+    std::unique_ptr<Gizmo> Clone() const
+    {
+        return h2::ToUnique(new Gizmo(*this));
+    }
+
+    int Data() const noexcept override { return data_; }
+    int data_;
+};
+
+struct NoncopyableWidget : WidgetBase
+{
+    int Data() const noexcept override { return 13; }
+};
+
+std::unique_ptr<WidgetBase> CopyGizmo(WidgetBase const& obj)
+{
+    auto const& gizmo = dynamic_cast<Gizmo const&>(obj);
+    return gizmo.Clone();
+}
+
+std::unique_ptr<WidgetBase> CopyWidget(WidgetBase const& obj)
+{
+    auto const& widget = dynamic_cast<Widget const&>(obj);
+    return h2::ToUnique(widget.Copy());
+}
+
+}// namespace <anon>
+
+TEST_CASE("testing the copy factory class", "[factory][utilities]")
+{
+    using WidgetFactory
+        = h2::factory::CopyFactory<WidgetBase>;
+
+    WidgetFactory factory;
+    SECTION("Register new classes")
+    {
+        CHECK(factory.register_builder(typeid(Gizmo), CopyGizmo));
+        CHECK(factory.register_builder(typeid(Widget), CopyWidget));
+        CHECK(factory.size() == 2UL);
+
+        SECTION("Re-registering a type fails.")
+        {
+            CHECK_FALSE(factory.register_builder(typeid(Widget), CopyWidget));
+            CHECK(factory.size() == 2UL);
+        }
+
+        SECTION("Copy objects by concrete type")
+        {
+            Widget w(17);
+            auto w2 = factory.copy_object(w);
+            auto const& w2_ref = *w2;
+
+            CHECK(w2->Data() == w.Data());
+            CHECK(typeid(w2_ref) == typeid(w));
+
+            Gizmo g(13);
+            auto g2 = factory.copy_object(g);
+            auto const& g2_ref = *g2;
+
+            CHECK(g2->Data() == g.Data());
+            CHECK(typeid(g2_ref) == typeid(g));
+        }
+
+        SECTION("Cannot copy unregistered widgets")
+        {
+            NoncopyableWidget ncw;
+            CHECK_THROWS(factory.copy_object(ncw));
+        }
+
+        SECTION("Copy objects through base type")
+        {
+            auto g = std::unique_ptr<WidgetBase>(new Gizmo(37));
+            auto w = std::unique_ptr<WidgetBase>(new Widget(73));
+
+            auto w2 = factory.copy_object(*w);
+            auto g2 = factory.copy_object(*g);
+
+            CHECK(w2->Data() == w->Data());
+            CHECK(g2->Data() == g->Data());
+        }
+
+        SECTION("Get list of supported types")
+        {
+            auto names = factory.registered_types();
+
+            CHECK(names.size() == factory.size());
+        }
+
+        SECTION("Unregister types.")
+        {
+            CHECK(factory.unregister(typeid(Widget)));
+            CHECK(factory.size() == 1UL);
+
+            CHECK(factory.unregister(typeid(Gizmo)));
+            CHECK(factory.size() == 0UL);
+        }
+    }
+}
---
+++ test/unit_test/patterns/factory/unit_test_object_factory.cpp
@@ -0,0 +1,169 @@
+// @H2_LICENSE_TEXT@
+
+#include <catch2/catch.hpp>
+
+#include <h2/patterns/factory/ObjectFactory.hpp>
+#include <typeinfo>
+
+namespace
+{
+struct WidgetBase {
+    virtual ~WidgetBase() = default;
+};
+struct Widget : WidgetBase {};
+struct Gizmo : WidgetBase {};
+
+std::unique_ptr<WidgetBase> MakeWidget()
+{
+    return std::unique_ptr<Widget>(new Widget);
+}
+
+std::unique_ptr<WidgetBase> MakeGizmo()
+{
+    return std::unique_ptr<Gizmo>(new Gizmo);
+}
+
+std::unique_ptr<WidgetBase> MakeGizmo2()
+{
+    return std::unique_ptr<Gizmo>(new Gizmo);
+}
+
+// What follows is an abstraction to allow testing multiple types of
+// keys. Currently tested types are string and int. This should be
+// sufficient for establishing that the invariants hold.
+
+enum class GenericKey
+{
+    INVALID,
+    WIDGET,
+    GIZMO
+};
+
+template <typename T> struct Key;
+
+template <>
+struct Key<std::string>
+{
+    static std::string get(GenericKey key)
+    {
+        switch (key)
+        {
+        case GenericKey::WIDGET:
+            return "Widget";
+        case GenericKey::GIZMO:
+            return "Gizmo";
+        case GenericKey::INVALID:
+            return "Invalid";
+        }
+        return "";
+    }
+};
+
+template <>
+struct Key<int>
+{
+    static int get(GenericKey key) noexcept
+    {
+        return static_cast<int>(key);
+    }
+};
+
+}// namespace <anon>
+
+TEMPLATE_TEST_CASE(
+    "testing the factory class", "[factory][utilities]", std::string, int)
+{
+    using WidgetFactory
+        = h2::factory::ObjectFactory<WidgetBase, TestType>;
+    using key = Key<TestType>;
+
+    WidgetFactory factory;
+
+    SECTION("New builders are registered")
+    {
+        CHECK(factory.register_builder(
+                  key::get(GenericKey::WIDGET), MakeWidget));
+
+        CHECK(factory.register_builder(
+                  key::get(GenericKey::GIZMO), MakeGizmo));
+
+        CHECK(factory.size() == 2UL);
+
+        // Verify the keys
+        auto names = factory.registered_keys();
+        for (auto const& name : names)
+            CHECK((name == key::get(GenericKey::WIDGET)
+                     || name == key::get(GenericKey::GIZMO)));
+
+    }
+
+    SECTION("A key is used multiple times")
+    {
+        CHECK(factory.register_builder(
+                  key::get(GenericKey::GIZMO), MakeGizmo));
+
+        CHECK_FALSE(factory.register_builder(
+                        key::get(GenericKey::GIZMO), MakeGizmo2));
+
+        CHECK(factory.size() == 1UL);
+    }
+
+    SECTION("A new object is requested with a valid key")
+    {
+        CHECK(factory.register_builder(
+                  key::get(GenericKey::WIDGET), MakeWidget));
+
+        CHECK(factory.register_builder(
+                  key::get(GenericKey::GIZMO), MakeGizmo));
+
+        std::unique_ptr<WidgetBase> g, w;
+
+        REQUIRE_NOTHROW(w = factory.create_object(key::get(GenericKey::WIDGET)));
+        auto const& w_ref = *w;
+        CHECK(typeid(w_ref) == typeid(Widget));
+
+        REQUIRE_NOTHROW(g = factory.create_object(key::get(GenericKey::GIZMO)));
+        auto const& g_ref = *g;
+        CHECK(typeid(g_ref) == typeid(Gizmo));
+    }
+
+    SECTION("A new object is requested with with an invalid key")
+    {
+        CHECK_THROWS_AS(factory.create_object(key::get(GenericKey::INVALID)),
+                        std::exception);
+    }
+
+    SECTION("Keys are removed")
+    {
+        CHECK(factory.register_builder(
+                  key::get(GenericKey::WIDGET), MakeWidget));
+        CHECK(factory.register_builder(
+                  key::get(GenericKey::GIZMO), MakeGizmo));
+
+        CHECK(factory.unregister(key::get(GenericKey::WIDGET)));
+        CHECK(factory.size() == 1UL);
+
+        SECTION("The remaining key is correct.")
+        {
+            auto names = factory.registered_keys();
+            CHECK(std::distance(names.begin(), names.end()) == 1UL);
+            CHECK(names.front() == key::get(GenericKey::GIZMO));
+        }
+
+        SECTION("The remaining builder still works.")
+        {
+            std::unique_ptr<WidgetBase> g;
+            CHECK_NOTHROW(g = factory.create_object(key::get(GenericKey::GIZMO)));
+            auto const& g_ref = *g;
+            CHECK(typeid(g_ref) == typeid(Gizmo));
+        }
+
+        SECTION("The removed key is invalid.")
+        {
+            std::unique_ptr<WidgetBase> obj;
+            CHECK_THROWS_AS(
+                obj = factory.create_object(key::get(GenericKey::WIDGET)),
+                std::exception);
+        }
+    }
+}
