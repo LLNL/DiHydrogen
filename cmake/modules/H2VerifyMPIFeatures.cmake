7a1c96b8de611684f9c5fd102446192b7e712818
Thomas R. Benson <benson31@llnl.gov>
2020-01-27 14:48:50 -0800

    add license text to all files

--- cmake/modules/H2VerifyMPIFeatures.cmake
+++ cmake/modules/H2VerifyMPIFeatures.cmake
@@ -1,4 +1,9 @@
-# @H2_LICENSE_TEXT@
+################################################################################
+## Copyright 2019-2020 Lawrence Livermore National Security, LLC and other
+## DiHydrogen Project Developers. See the top-level LICENSE file for details.
+##
+## SPDX-License-Identifier: Apache-2.0
+################################################################################
 
 if (NOT MPI_CXX_FOUND)
   find_package(MPI COMPONENT CXX REQUIRED)
