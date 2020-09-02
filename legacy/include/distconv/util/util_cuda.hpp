3f8f6b2bcf2abdeffd2e9a3360ab3b6cd0928bf5
Brian C. Van Essen <vanessen1@llnl.gov>
2020-09-02 15:05:11 -0700

    Added psuedo-atomicAdd for double data types on devices with cuda capability <6.x

--- legacy/include/distconv/util/util_cuda.hpp
+++ legacy/include/distconv/util/util_cuda.hpp
@@ -39,6 +39,24 @@
 #define DISTCONV_CUDA_MALLOC(p, s)                              \
   distconv::util::cuda_malloc((void**)p, s, __FILE__, __LINE__)
 
+#if __CUDA_ARCH__ < 600
+inline __device__ double atomicAdd(double* address, double val)
+{
+    unsigned long long int* address_as_ull =
+      (unsigned long long int*)address;
+    unsigned long long int old = *address_as_ull, assumed;
+    do {
+      assumed = old;
+      old = atomicCAS(address_as_ull, assumed,
+                      __double_as_longlong(val +
+                                           __longlong_as_double(assumed)));
+      // Note: uses integer comparison to avoid hang in case of NaN
+      // (since NaN != NaN)
+    } while (assumed != old);
+    return __longlong_as_double(old);
+}
+#endif
+
 namespace distconv {
 namespace util {
 
