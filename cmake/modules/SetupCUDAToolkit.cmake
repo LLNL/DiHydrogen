09665c53d077af8ec929d10f99f0ac6df1ec492f
Thomas R. Benson <benson31@llnl.gov>
2022-02-09 17:47:57 -0500

    Some updates

--- cmake/modules/SetupCUDAToolkit.cmake
+++ cmake/modules/SetupCUDAToolkit.cmake
@@ -5,47 +5,32 @@
 ## SPDX-License-Identifier: Apache-2.0
 ################################################################################
 
-# Copied from LBANN
+find_package(CUDAToolkit 11.0.0 REQUIRED)
+find_package(cuDNN REQUIRED)
 
-find_package(CUDA REQUIRED)
-if (NOT DEFINED CMAKE_CUDA_STANDARD)
-  set(CMAKE_CUDA_STANDARD 14)
-endif ()
-set(CMAKE_CUDA_STANDARD_REQUIRED TRUE)
-
-find_package(NVTX REQUIRED)
-
-if (NOT TARGET cuda::toolkit)
-  add_library(cuda::toolkit INTERFACE IMPORTED)
+if (NOT TARGET h2::cuda_toolkit)
+  add_library(h2::cuda_toolkit INTERFACE IMPORTED)
 endif ()
 
-set_property(TARGET cuda::toolkit APPEND PROPERTY
-  INTERFACE_COMPILE_OPTIONS $<$<COMPILE_LANGUAGE:CUDA>:
-  -g --expt-extended-lambda>)
+target_compile_options(
+  h2::cuda_toolkit
+  INTERFACE
+  $<$<COMPILE_LANGUAGE:CUDA>:-g>
+  $<$<COMPILE_LANGUAGE:CUDA>:--expt-extended-lambda>
+  $<$<AND:$<COMPILE_LANGUAGE:CUDA>,$<CONFIG:Debug>>:--device-debug>)
 
-set_property(TARGET cuda::toolkit APPEND PROPERTY
-  INTERFACE_COMPILE_OPTIONS $<$<AND:$<COMPILE_LANGUAGE:CUDA>,$<CONFIG:Debug>>:--device-debug>)
+target_include_directories(
+  h2::cuda_toolkit
+  INTERFACE
+  "${CUDA_INCLUDE_DIRS}")
 
-set_property(TARGET cuda::toolkit APPEND PROPERTY
-  INTERFACE_INCLUDE_DIRECTORIES "${CUDA_INCLUDE_DIRS}")
+target_link_libraries(
+  h2::cuda_toolkit
+  INTERFACE
+  h2::cuDNN
+  CUDA::nvToolsExt
+  CUDA::cuda_driver
+  CUDA::cudart)
 
-set_property(TARGET cuda::toolkit APPEND PROPERTY
-  INTERFACE_LINK_LIBRARIES ${CUDA_LIBRARIES} cuda cuda::nvtx)
-
-# Set the target PTX and architecture version. If the auto detection is successful, CMake should
-# return a list of length 2, e.., "-gencode:arch=compute_62,code=sm_62". Otherwise, it should enumerate
-# all the version numbers, however, adding multiple '-gencode' options with set_property does not seem
-# work. More fundamentally, it's not desirable to generate ptx and binary codes for all versions since
-# that would inflate compilation time. The code below checks whether automatic detection is successful,
-# and only then add the options to the nvcc command-line option list.
-
-cuda_select_nvcc_arch_flags(ARCH_FLAGS Auto)
-
-list(LENGTH ARCH_FLAGS arch_flag_length)
-if (${arch_flag_length} GREATER 2)
-  message(STATUS "Ignoring ${ARCH_FLAGS}")
-else ()
-  message(STATUS "Targeting CUDA architecture version ${ARCH_FLAGS_readable}")
-  set_property(TARGET cuda::toolkit APPEND PROPERTY
-    INTERFACE_COMPILE_OPTIONS $<$<COMPILE_LANGUAGE:CUDA>:${ARCH_FLAGS}>)
-endif ()
+# Arch flags are now set automatically. Be sure to set
+# CMAKE_CUDA_ARCHITECTURES on the command line.
