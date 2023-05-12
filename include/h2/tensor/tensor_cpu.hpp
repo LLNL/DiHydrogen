b242a6bf05ec289ea97657264fe861bc23736984
Nikoli Dryden <ndryden@users.noreply.github.com>
2023-05-12 15:16:12 -0700

    Basic CPU tensors, changed APIs, some code reorg.

---
+++ include/h2/tensor/tensor_cpu.hpp
@@ -0,0 +1,145 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright 2019-2022 Lawrence Livermore National Security, LLC and other
+// DiHydrogen Project Developers. See the top-level LICENSE file for details.
+//
+// SPDX-License-Identifier: Apache-2.0
+////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+/** @file
+ *
+ * Local tensors that live on CPUs.
+ */
+
+#include "h2/tensor/tensor.hpp"
+#include "h2/tensor/strided_memory.hpp"
+#include "h2/tensor/tensor_types.hpp"
+#include "strided_memory.hpp"
+#include "tensor_types.hpp"
+
+namespace h2 {
+
+template <typename T>
+class Tensor<T, Device::CPU> : public BaseTensor<T> {
+public:
+  using value_type = T;
+  static constexpr Device device = Device::CPU;
+
+  Tensor(ShapeTuple shape_, DimensionTypeTuple dim_types_) :
+    BaseTensor<T>(shape_, dim_types_),
+    tensor_memory(shape_)
+  {}
+
+  Tensor() : Tensor(ShapeTuple(), DimensionTypeTuple()) {}
+
+  Tensor(T* buffer, ShapeTuple shape_, DimensionTypeTuple dim_types_,
+         StrideTuple strides_) :
+    BaseTensor<T>(ViewType::Mutable, shape_, dim_types_),
+    tensor_memory(buffer, shape_, strides_)
+  {}
+
+  Tensor(const T* buffer, ShapeTuple shape_, DimensionTypeTuple dim_types_,
+         StrideTuple strides_) :
+    BaseTensor<T>(ViewType::Const, shape_, dim_types_),
+    tensor_memory(const_cast<T*>(buffer), shape_, strides_)
+  {}
+
+  StrideTuple strides() const H2_NOEXCEPT override {
+    return tensor_memory.strides();
+  }
+
+  typename StrideTuple::type strides(typename StrideTuple::size_type i) const H2_NOEXCEPT override {
+    return tensor_memory.strides()[i];
+  }
+
+  bool is_contiguous() const H2_NOEXCEPT override {
+    return are_strides_contiguous(tensor_memory.strides());
+  }
+
+  Device get_device() const H2_NOEXCEPT override { return device; }
+
+  void empty() override {
+    if (this->is_view()) {
+      throw H2Exception("Cannot empty a view");
+    }
+    tensor_memory = StridedMemory<T, Device::CPU>();
+    this->tensor_shape = ShapeTuple();
+    this->tensor_dim_types = DimensionTypeTuple();
+  }
+
+  void resize(ShapeTuple new_shape) override {
+    if (this->is_view()) {
+      throw H2Exception("Cannot resize a view");
+    }
+    if (new_shape.size() > this->tensor_shape.size()) {
+      throw H2Exception("Must provide dimension types to resize larger");
+    }
+    tensor_memory = StridedMemory<T, Device::CPU>(new_shape);
+    this->tensor_shape = new_shape;
+    this->tensor_dim_types.set_size(new_shape.size());
+  }
+
+  void resize(ShapeTuple new_shape, DimensionTypeTuple new_dim_types) override {
+    if (this->is_view()) {
+      throw H2Exception("Cannot resize a view");
+    }
+    tensor_memory = StridedMemory<T, Device::CPU>(new_shape);
+    this->tensor_shape = new_shape;
+    this->tensor_dim_types = new_dim_types;
+  }
+
+  T* data() override {
+    if (this->tensor_view_type == ViewType::Const) {
+      throw H2Exception("Cannot access non-const buffer of const view");
+    }
+    return tensor_memory.data();
+  }
+
+  const T* const_data() const override {
+    return tensor_memory.const_data();
+  }
+
+  Tensor<T, Device::CPU>* view() override {
+    return view(CoordTuple(TuplePad<CoordTuple>(this->tensor_shape.size(), ALL)));
+  }
+
+  Tensor<T, Device::CPU>* view(CoordTuple coords) override {
+    return new Tensor<T, Device::CPU>(
+      ViewType::Mutable,
+      tensor_memory,
+      get_range_shape(coords, this->tensor_shape),
+      filter_by_trivial(coords, this->tensor_dim_types),
+      coords);
+  }
+
+  const Tensor<T, Device::CPU>* const_view() const override {
+    return const_view(CoordTuple(TuplePad<CoordTuple>(this->tensor_shape.size(), ALL)));
+  }
+
+  const Tensor<T, Device::CPU>* const_view(CoordTuple coords) const override {
+    return new Tensor<T, Device::CPU>(
+      ViewType::Const,
+      tensor_memory,
+      get_range_shape(coords, this->tensor_shape),
+      filter_by_trivial(coords, this->tensor_dim_types),
+      coords);
+  }
+
+  T get(SingleCoordTuple coords) const override {
+    return *(tensor_memory.get(coords));
+  }
+
+private:
+  /** Underlying memory buffer for the tensor. */
+  StridedMemory<T, Device::CPU> tensor_memory;
+
+  /** Private constructor for views. */
+  Tensor(ViewType view_type_, const StridedMemory<T, Device::CPU>& mem_,
+         ShapeTuple shape_, DimensionTypeTuple dim_types_, CoordTuple coords) :
+    BaseTensor<T>(view_type_, shape_, dim_types_),
+    tensor_memory(mem_, coords)
+  {}
+};
+
+}  // namespace h2
---
+++ include/h2/tensor/tensor_types.hpp
@@ -0,0 +1,192 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright 2019-2022 Lawrence Livermore National Security, LLC and other
+// DiHydrogen Project Developers. See the top-level LICENSE file for details.
+//
+// SPDX-License-Identifier: Apache-2.0
+////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include <array>
+#include <cstdint>
+
+#include <El.hpp>
+
+#include "h2/tensor/fixed_size_tuple.hpp"
+
+/** @file
+ *
+ * Various types and helpers for defining tensors.
+ */
+
+namespace h2 {
+
+/**
+ * Indicates the type of a tensor dimension.
+ *
+ * A tensor dimension type is a tag that indicates the semantics of a
+ * given dimension. This is used to construct mappings to certain
+ * algorithms which associate particular semantics to dimensions (e.g.,
+ * the sample dimension).
+ *
+ * These are not used for other sorts of correctness checking.
+ */
+enum class DimensionType {
+  Any,  /**< Catch-all, does not ascribe particular semantics. */
+  Sample,  /**< The sample ("batch") dimension. */
+  Channel,  /**< The channel ("feature") dimension in convolutions. */
+  Filter,  /**< The filter dimension in convolutions. */
+  Spatial,  /**< The spatial (height, width, depth, etc.) dimension(s). */
+  Sequence  /**< The sequence dimension (e.g., in textual data). */
+};
+
+using DT = DimensionType;  // Alias to save you some typing.
+
+/**
+ * Compute device type (e.g., CPU, GPU).
+ */
+using Device = El::Device;  // Leverage Hydrogen's device typing.
+
+/**
+ * Integer type used for the number of dimensions.
+ */
+using NDimType = std::int32_t;
+
+/**
+ * Integer type used for storing dimensions.
+ */
+using DimType = std::int32_t;
+
+/**
+ * Integer type used for data indices.
+ */
+using DataIndexType = std::int64_t;
+
+/**
+ * Maximum number of dimensions a tensor may have.
+ */
+static constexpr NDimType MAX_TENSOR_DIMS = 8;
+
+/**
+ * Fixed-size tuple where the SizeType is NDimType and the max size is
+ * MAX_TENSOR_DIMS.
+ */
+template <typename T>
+using NDimTuple = FixedSizeTuple<T, NDimType, MAX_TENSOR_DIMS>;
+
+/**
+ * The shape of a tensor, a tuple with some number of integral values.
+ */
+using ShapeTuple = NDimTuple<DimType>;
+
+/**
+ * Tuple of dimension types.
+ */
+using DimensionTypeTuple = NDimTuple<DimensionType>;
+
+using DTTuple = DimensionTypeTuple;  // Alias to save you some typing.
+
+/**
+ * The strides of a tensor.
+ */
+using StrideTuple = NDimTuple<DataIndexType>;
+
+/**
+ * Represents a contiguous range [start, end).
+ */
+struct DimensionRange {
+  DimType start;  /**< Start of a range. */
+  DimType end;  /**< End of a range. */
+
+  constexpr DimensionRange() : start(0), end(0) {}
+  constexpr DimensionRange(DimType i) : start(i), end(i+1) {}
+  constexpr DimensionRange(DimType start_, DimType end_) : start(start_), end(end_) {}
+
+  constexpr bool operator==(const DimensionRange& other) H2_NOEXCEPT {
+    return start == other.start && end == other.end;
+  }
+};
+
+/**
+ * Tuple of dimension ranges.
+ */
+using CoordTuple = NDimTuple<DimensionRange>;
+
+/** Special DimensionRange that represents a entire range. */
+static constexpr DimensionRange ALL(0, -1);
+
+/**
+ * Tuple of exact coordinates.
+ */
+using SingleCoordTuple = NDimTuple<DimType>;
+
+/**
+ * Return the coordinates of the initial point of coordinate range.
+ */
+constexpr inline SingleCoordTuple get_range_start(CoordTuple coords) H2_NOEXCEPT {
+  SingleCoordTuple coords_start(TuplePad<SingleCoordTuple>(coords.size()));
+  for (typename CoordTuple::size_type i = 0; i < coords.size(); ++i) {
+    // Abuse the fact that ALL.start = 0.
+    coords_start[i] = coords[i].start;
+  }
+  return coords_start;
+}
+
+/**
+ * Return true if the DimensionRange is trivial.
+ */
+constexpr inline bool is_coord_trivial(DimensionRange coord) H2_NOEXCEPT {
+  return coord.start + 1 == coord.end;
+}
+
+/**
+ * Return the shape defined by a coordinate range within a larger shape,
+ * eliminating trivial dimensions.
+ */
+constexpr inline ShapeTuple get_range_shape(CoordTuple coords, ShapeTuple shape) H2_NOEXCEPT {
+  H2_ASSERT_DEBUG(coords.size() <= shape.size(),
+                  "coords size not compatible with shape size");
+  ShapeTuple new_shape(TuplePad<ShapeTuple>(shape.size()));
+  typename ShapeTuple::size_type j = 0;
+  for (typename ShapeTuple::size_type i = 0; i < shape.size(); ++i) {
+    if (i >= coords.size() || coords[i] == ALL) {
+      new_shape[j] = shape[i];
+      ++j;
+    } else if (!is_coord_trivial(coords[i])) {
+      new_shape[j] = coords[i].end - coords[i].start;
+      ++j;
+    }
+  }
+  new_shape.set_size(j);
+  return new_shape;
+}
+
+/**
+ * Return a new tuple that consists of the entries in the original at
+ * indices where coords is not trivial.
+ */
+template <typename T, typename SizeType, SizeType N>
+constexpr inline FixedSizeTuple<T, SizeType, N> filter_by_trivial(
+  CoordTuple coords, FixedSizeTuple<T, SizeType, N> tuple) H2_NOEXCEPT {
+  FixedSizeTuple<T, SizeType, N> new_tuple(FixedSizeTuplePadding<T, SizeType>(tuple.size()));
+  SizeType j = 0;
+  for (SizeType i = 0; i < tuple.size(); ++i) {
+    if (i >= coords.size() || !is_coord_trivial(coords[i])) {
+      new_tuple[j] = tuple[i];
+      ++j;
+    }
+  }
+  new_tuple.set_size(j);
+  return new_tuple;
+}
+
+/**
+ * Specifies the type of view.
+ */
+enum class ViewType {
+  None,  /**< Not a view. */
+  Mutable,  /**< A view that can modify the original. */
+  Const  /**< A view that cannot modify the original. */
+};
+
+}  // namespace h2
