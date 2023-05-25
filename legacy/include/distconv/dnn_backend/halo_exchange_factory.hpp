535d52081b1ab10e61fc6f092b28a2e822136452
Tom Benson <benson31@llnl.gov>
2023-05-25 07:42:10 -0700

    Update license and add where needed

--- legacy/include/distconv/dnn_backend/halo_exchange_factory.hpp
+++ legacy/include/distconv/dnn_backend/halo_exchange_factory.hpp
@@ -1,3 +1,10 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright 2019-2023 Lawrence Livermore National Security, LLC and other
+// DiHydrogen Project Developers. See the top-level LICENSE file for details.
+//
+// SPDX-License-Identifier: Apache-2.0
+////////////////////////////////////////////////////////////////////////////////
+
 #pragma once
 
 #include "distconv/tensor/halo_exchange_cuda.hpp"
--- legacy/src/dnn_backend/streams_manager.cpp
+++ legacy/src/dnn_backend/streams_manager.cpp
@@ -1,3 +1,10 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright 2019-2023 Lawrence Livermore National Security, LLC and other
+// DiHydrogen Project Developers. See the top-level LICENSE file for details.
+//
+// SPDX-License-Identifier: Apache-2.0
+////////////////////////////////////////////////////////////////////////////////
+
 #include "distconv/dnn_backend/dnn_backend.hpp"
 #include "distconv/util/util_gpu.hpp"
 #include "distconv_config.hpp"
