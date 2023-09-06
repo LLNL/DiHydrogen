0e56dc6aa6d6aea3c81eeb9eaa90e6a287b63925
Tom Benson <benson31@llnl.gov>
2023-09-06 15:57:16 -0400

    Use the default H2 cub allocator in distconv - cuda edition

--- legacy/include/distconv/runtime_cuda.hpp
+++ legacy/include/distconv/runtime_cuda.hpp
@@ -13,23 +13,20 @@ class CUDADeviceMemoryPool {
   void *get(size_t size, cudaStream_t st);
   void release(void *p);
   size_t get_max_allocatable_size(size_t limit);
-  
- protected:
-  cub::CachingDeviceAllocator m_allocator;
 };
 
 class RuntimeCUDA {
  public:
   static CUDADeviceMemoryPool &get_device_memory_pool();
   static cudaEvent_t &get_event(int idx=0);
-  
+
  protected:
   static RuntimeCUDA *m_instance;
   //PinnedMemoryPool m_pmp;
   CUDADeviceMemoryPool m_dmp;
   static constexpr int m_num_events = 2;
   cudaEvent_t m_events[m_num_events];
-  
+
   RuntimeCUDA();
   static RuntimeCUDA &get_instance();
 };
