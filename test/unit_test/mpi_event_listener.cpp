f58bdbdfcc06ad5cef26f7f591af6d43862eb26d
Nikoli Dryden <ndryden@users.noreply.github.com>
2024-03-20 21:40:57 -0700

    Avoid test hangs after failures when using `for_comms`.
    Also add `get_comm_or_skip` as an occasionally helpful utility.

---
+++ test/unit_test/mpi_event_listener.cpp
@@ -0,0 +1,71 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright 2019-2024 Lawrence Livermore National Security, LLC and other
+// DiHydrogen Project Developers. See the top-level LICENSE file for details.
+//
+// SPDX-License-Identifier: Apache-2.0
+////////////////////////////////////////////////////////////////////////////////
+
+#include "h2/tensor/dist_types.hpp"
+
+#include <catch2/reporters/catch_reporter_event_listener.hpp>
+#include <catch2/reporters/catch_reporter_registrars.hpp>
+
+
+namespace internal
+{
+
+// These indicate when we are actually performing a blocking global
+// operation.
+
+bool in_for_comms = false;
+
+void start_for_comms()
+{
+  in_for_comms = true;
+}
+
+void end_for_comms()
+{
+  in_for_comms = false;
+}
+
+}  // namespace internal
+
+
+/**
+ * Gracefully handle test case failures when not all ranks may see
+ * the failure. This is intended for use with the `for_comms` utility
+ * (or similar).
+ */
+class MPIEventListener final : public Catch::EventListenerBase
+{
+public:
+  using Catch::EventListenerBase::EventListenerBase;
+
+  void testCasePartialEnded(const Catch::TestCaseStats& test_case_stats,
+                            uint64_t) final
+  {
+    if (!internal::in_for_comms)
+    {
+      // Not in for_comms, nothing to do.
+      return;
+    }
+
+    // for_comms will join the allreduce from successful or
+    // non-participating ranks.
+    // We let Catch2 handle the rest of the failure as usual.
+    if (!test_case_stats.totals.assertions.allOk())
+    {
+      int test_result = 0;
+      El::mpi::AllReduce(&test_result,
+                         1,
+                         El::mpi::MIN,
+                         El::mpi::COMM_WORLD,
+                         El::SyncInfo<El::Device::CPU>{});
+      // Indicate we are done with the for_comms.
+      internal::end_for_comms();
+    }
+  }
+};
+
+CATCH_REGISTER_LISTENER(MPIEventListener);
