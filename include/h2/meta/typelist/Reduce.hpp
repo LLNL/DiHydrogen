d094cc8992d0de96463f368a9ca0e0c7ede3aef0
Nikoli Dryden <ndryden@users.noreply.github.com>
2024-08-14 11:51:20 -0700

    Document typelist Reduce and add some logical reduction ops.

--- include/h2/meta/typelist/Reduce.hpp
+++ include/h2/meta/typelist/Reduce.hpp
@@ -5,8 +5,9 @@
 // SPDX-License-Identifier: Apache-2.0
 ////////////////////////////////////////////////////////////////////////////////
 
-#ifndef H2_META_TYPELIST_REDUCE_HPP_
-#define H2_META_TYPELIST_REDUCE_HPP_
+#pragma once
+
+#include <type_traits>
 
 #include "LispAccessors.hpp"
 #include "TypeList.hpp"
@@ -19,11 +20,15 @@ namespace meta
 namespace tlist
 {
 
+/** @brief Apply a left fold on a type list. */
 template <template <class,class> class F, typename Acc, typename List>
 struct FoldlTLT;
 
+/** @brief Apply a left fold on a type list. */
 template <template <class,class> class F, typename Acc, typename List>
 using FoldlTL = Force<FoldlTLT<F, Acc, List>>;
+
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
 
 template <template <class,class> class F, typename Acc>
 struct FoldlTLT<F, Acc, Empty>
@@ -37,11 +42,17 @@ struct FoldlTLT<F, Acc, TL<Ts...>>
     using type = FoldlTL<F, Force<F<Acc, Car<TL<Ts...>>>>, Cdr<TL<Ts...>>>;
 };
 
+#endif // DOXYGEN_SHOULD_SKIP_THIS
+
+/** @brief Apply a right fold on a type list. */
 template <template <class,class> class F, typename Acc, typename... Ts>
 struct FoldrTLT;
 
+/** @brief Apply a right fold on a type list. */
 template <template <class,class> class F, typename Acc, typename List>
 using FoldrTL = Force<FoldrTLT<F, Acc, List>>;
+
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
 
 template <template <class,class> class F, typename Acc>
 struct FoldrTLT<F, Acc, Empty>
@@ -55,7 +66,33 @@ struct FoldrTLT<F, Acc, TL<Ts...>>
     using type = FoldrTL<F, Force<F<Car<TL<Ts...>>, Acc>>, Cdr<TL<Ts...>>>;
 };
 
-} // namespace tlist
-} // namespace meta
-} // namespace h2
-#endif // H2_META_TYPELIST_REDUCE_HPP_
+#endif  // DOXYGEN_SHOULD_SKIP_THIS
+
+}  // namespace tlist
+
+/** @brief Logical And between two types. */
+template <typename T, typename U>
+struct And;
+
+/** @brief Logical Or between two types. */
+template <typename T, typename U>
+struct Or;
+
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
+
+template <bool A, bool B>
+struct And<std::bool_constant<A>, std::bool_constant<B>>
+{
+  using type = std::bool_constant<A && B>;
+};
+
+template <bool A, bool B>
+struct Or<std::bool_constant<A>, std::bool_constant<B>>
+{
+  using type = std::bool_constant<A || B>;
+};
+
+#endif  // DOXYGEN_SHOULD_SKIP_THIS
+
+}  // namespace meta
+}  // namespace h2
