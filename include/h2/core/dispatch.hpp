6cd493d7a24bee78d3526a6b3b93e90788eb8b20
Nikoli Dryden <ndryden@users.noreply.github.com>
2024-08-14 18:22:34 -0700

    Add helper macros for some static dispatch cases.

--- include/h2/core/dispatch.hpp
+++ include/h2/core/dispatch.hpp
@@ -72,7 +72,58 @@
  * the device type (`CPUDev_t`, etc.).
  *
  * See below for an example of this (used also in our unit testing).
+ *
+ * Dynamic (runtime) dispatch:
+ *
+ * Documentation TODO.
  */
+
+#define H2_INSTANTIATE_DEV_1(device)            \
+  PROTO(device, float);                         \
+  PROTO(device, double);                        \
+  PROTO(device, std::int32_t);                  \
+  PROTO(device, std::uint32_t);
+#define H2_INSTANTIATE_DEV_2(device)            \
+  PROTO(device, float, float);                  \
+  PROTO(device, float, double);                 \
+  PROTO(device, float, std::int32_t);           \
+  PROTO(device, float, std::uint32_t);          \
+  PROTO(device, double, float);                 \
+  PROTO(device, double, double);                \
+  PROTO(device, double, std::int32_t);          \
+  PROTO(device, double, std::uint32_t);         \
+  PROTO(device, std::int32_t, float);           \
+  PROTO(device, std::int32_t, double);          \
+  PROTO(device, std::int32_t, std::int32_t);    \
+  PROTO(device, std::int32_t, std::uint32_t);   \
+  PROTO(device, std::uint32_t, float);          \
+  PROTO(device, std::uint32_t, double);         \
+  PROTO(device, std::uint32_t, std::int32_t);   \
+  PROTO(device, std::uint32_t, std::uint32_t);
+
+#define H2_INSTANTIATE_CPU_1 H2_INSTANTIATE_DEV_1(CPUDev_t)
+#define H2_INSTANTIATE_CPU_2 H2_INSTANTIATE_DEV_2(CPUDev_t)
+
+#ifdef H2_HAS_GPU
+
+#define H2_INSTANTIATE_GPU_1 H2_INSTANTIATE_DEV_1(GPUDev_t)
+#define H2_INSTANTIATE_GPU_2 H2_INSTANTIATE_DEV_2(GPUDev_t)
+
+#define H2_INSTANTIATE_1                        \
+  H2_INSTANTIATE_CPU_1                          \
+  H2_INSTANTIATE_GPU_1
+
+#define H2_INSTANTIATE_2                        \
+  H2_INSTANTIATE_CPU_2                          \
+  H2_INSTANTIATE_GPU_2
+
+#else  // H2_HAS_GPU
+
+#define H2_INSTANTIATE_1 H2_INSTANTIATE_CPU_1
+
+#define H2_INSTANTIATE_2 H2_INSTANTIATE_CPU_2
+
+#endif  // H2_HAS_GPU
 
 namespace h2
 {
--- src/core/dispatch.cpp
+++ src/core/dispatch.cpp
@@ -22,19 +22,9 @@ void dispatch_test_impl(GPUDev_t, T* v) {}
 #endif
 
 // Instantiate for all compute types:
-
-
-template void dispatch_test_impl<float>(CPUDev_t, float*);
-template void dispatch_test_impl<double>(CPUDev_t, double*);
-template void dispatch_test_impl<std::int32_t>(CPUDev_t, std::int32_t*);
-template void dispatch_test_impl<std::uint32_t>(CPUDev_t, std::uint32_t*);
-
-#ifdef H2_HAS_GPU
-template void dispatch_test_impl<float>(GPUDev_t, float*);
-template void dispatch_test_impl<double>(GPUDev_t, double*);
-template void dispatch_test_impl<std::int32_t>(GPUDev_t, std::int32_t*);
-template void dispatch_test_impl<std::uint32_t>(GPUDev_t, std::uint32_t*);
-#endif
+#define PROTO(device, t1) template void dispatch_test_impl<t1>(device, t1*)
+H2_INSTANTIATE_1
+#undef PROTO
 
 }  // namespace impl
 
