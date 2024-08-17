444bd82fec55abb4d50a325d2e6c25dcf68e8e6e
Nikoli Dryden <ndryden@users.noreply.github.com>
2024-08-17 15:47:26 -0700

    Add a helper for downcasting through a `unique_ptr`.

---
+++ include/h2/utils/unique_ptr_cast.hpp
@@ -0,0 +1,38 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright 2019-2024 Lawrence Livermore National Security, LLC and other
+// DiHydrogen Project Developers. See the top-level LICENSE file for details.
+//
+// SPDX-License-Identifier: Apache-2.0
+////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+/** @file
+ *
+ * Utilities for working with `std::unique_ptr`s.
+ */
+
+
+#include <memory>
+#include <type_traits>
+
+
+namespace h2
+{
+
+/**
+ * Cast a unique_ptr to a base class to a unique_ptr to a derived class
+ * of the base.
+ *
+ * The input unique_pointer will be invalidated.
+ */
+template <typename DerivedT, typename BaseT>
+std::unique_ptr<DerivedT> downcast_uptr(std::unique_ptr<BaseT>& p)
+{
+  static_assert(
+      std::is_base_of_v<BaseT, DerivedT>,
+      "Cannot cast a unique_ptr from a base class to a non-derived class");
+  return std::unique_ptr<DerivedT>(static_cast<DerivedT*>(p.release()));
+}
+
+}  // namespace h2
---
+++ test/unit_test/utils/unit_test_unique_ptr_cast.cpp
@@ -0,0 +1,36 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright 2019-2023 Lawrence Livermore National Security, LLC and other
+// DiHydrogen Project Developers. See the top-level LICENSE file for details.
+//
+// SPDX-License-Identifier: Apache-2.0
+////////////////////////////////////////////////////////////////////////////////
+
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/catch_template_test_macros.hpp>
+
+#include "h2/utils/unique_ptr_cast.hpp"
+#include "h2/tensor/tensor.hpp"
+#include "../tensor/utils.hpp"
+
+using namespace h2;
+
+
+TEMPLATE_LIST_TEST_CASE("unique_ptr_cast works with tensors",
+                        "[utilities][unique_ptr_cast]",
+                        AllDevComputeTypePairsList)
+{
+  constexpr Device Dev = meta::tlist::At<TestType, 0>::value;
+  using Type = meta::tlist::At<TestType, 1>;
+  using TensorType = Tensor<Type>;
+
+  std::unique_ptr<BaseTensor> base_ptr = std::make_unique<TensorType>(
+      Dev, ShapeTuple{4, 6}, DTTuple{DT::Sample, DT::Any});
+  void* orig_data = base_ptr->storage_data();
+  std::unique_ptr<TensorType> derived_ptr;
+  REQUIRE_NOTHROW([&]() {
+    derived_ptr = downcast_uptr<TensorType>(base_ptr);
+  }());
+  // Some additional sanity-checks:
+  REQUIRE(derived_ptr->shape() == ShapeTuple{4, 6});
+  REQUIRE(orig_data == static_cast<void*>(derived_ptr->data()));
+}
