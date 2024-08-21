a14f81e56e8a4af3646ebfab9e06b577fbac9380
Nikoli Dryden <ndryden@users.noreply.github.com>
2024-08-21 14:51:55 -0700

    Add version of `downcast_uptr` for pointers to const types.

--- include/h2/utils/unique_ptr_cast.hpp
+++ include/h2/utils/unique_ptr_cast.hpp
@@ -35,4 +35,14 @@ std::unique_ptr<DerivedT> downcast_uptr(std::unique_ptr<BaseT>& p)
   return std::unique_ptr<DerivedT>(static_cast<DerivedT*>(p.release()));
 }
 
+template <typename DerivedT, typename BaseT>
+std::unique_ptr<const DerivedT> downcast_uptr(std::unique_ptr<const BaseT>& p)
+{
+  static_assert(
+      std::is_base_of_v<BaseT, DerivedT>,
+      "Cannot cast a unique_ptr from a base class to a non-derived class");
+  return std::unique_ptr<const DerivedT>(
+      static_cast<const DerivedT*>(p.release()));
+}
+
 }  // namespace h2
