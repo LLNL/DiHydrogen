7c32cb5f24e501acda8989c0c1f15202c95110a6
Nikoli Dryden <ndryden@users.noreply.github.com>
2024-08-23 23:21:30 -0700

    Add function traits.

---
+++ include/h2/utils/function_traits.hpp
@@ -0,0 +1,80 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright 2019-2024 Lawrence Livermore National Security, LLC and other
+// DiHydrogen Project Developers. See the top-level LICENSE file for details.
+//
+// SPDX-License-Identifier: Apache-2.0
+////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+/** @file
+ *
+ * Traits describing functions.
+ */
+
+// This partially adapted from
+// https://stackoverflow.com/questions/7943525/is-it-possible-to-figure-out-the-parameter-type-and-return-type-of-a-lambda
+
+#include <functional>
+#include <tuple>
+
+#include <cstddef>
+
+
+namespace h2
+{
+
+/**
+ * Collect information on a function (anything with operator()).
+ */
+template <typename T>
+struct FunctionTraits
+    : public FunctionTraits<decltype(&std::remove_reference_t<T>::operator())>
+{};
+
+template <typename Ret, typename... Args>
+struct FunctionTraits<Ret(Args...)>
+{
+  using ArgsTuple = std::tuple<Args...>;
+  using RetT = Ret;
+  using FuncT = Ret(Args...);
+
+  /** Number of arguments the function takes. */
+  static constexpr std::size_t arity = sizeof...(Args);
+
+  /** Access the ith argument type. */
+  template <std::size_t i>
+  using arg = std::tuple_element_t<i, ArgsTuple>;
+};
+
+template <typename Ret, typename... Args>
+struct FunctionTraits<Ret(*)(Args...)> : public FunctionTraits<Ret(Args...)> {};
+template <typename FuncT>
+struct FunctionTraits<std::function<FuncT>> : public FunctionTraits<FuncT> {};
+template <typename T>
+struct FunctionTraits<T&> : public FunctionTraits<T> {};
+template <typename T>
+struct FunctionTraits<const T&> : public FunctionTraits<T> {};
+template <typename T>
+struct FunctionTraits<T&&> : public FunctionTraits<T> {};
+template <typename T>
+struct FunctionTraits<const T&&> : public FunctionTraits<T> {};
+template <typename T>
+struct FunctionTraits<T*> : public FunctionTraits<T> {};
+template <typename T>
+struct FunctionTraits<const T*> : public FunctionTraits<T> {};
+
+template <typename Class, typename Ret, typename... Args>
+struct FunctionTraits<Ret (Class::*)(Args...)>
+    : public FunctionTraits<Ret(Args...)>
+{
+  using ClassT = Class;
+};
+template <typename Class, typename Ret, typename... Args>
+struct FunctionTraits<Ret (Class::*)(Args...) const>
+    : public FunctionTraits<Ret(Args...)>
+{
+  using ClassT = Class;
+};
+
+}  // namespace h2
---
+++ test/static_test/utils/static_test_function_traits.cpp
@@ -0,0 +1,87 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright 2019-2024 Lawrence Livermore National Security, LLC and other
+// DiHydrogen Project Developers. See the top-level LICENSE file for details.
+//
+// SPDX-License-Identifier: Apache-2.0
+////////////////////////////////////////////////////////////////////////////////
+
+#include "h2/utils/function_traits.hpp"
+
+#include <type_traits>
+
+
+using namespace h2;
+
+
+int nullary() { return 42; }
+int unary(float) { return 42; }
+void unary_v(float) {}
+int binary(float, double) { return 42; }
+[[maybe_unused]] auto unary_lambda = [](float) -> int { return 42; };
+
+struct Test
+{
+  int nullary() { return 42; }
+  int unary(float) { return 42; }
+};
+
+struct NullaryFunctor
+{
+  int operator()() { return 42; }
+};
+struct UnaryFunctor
+{
+  int operator()(float) { return 42; }
+};
+
+static_assert(std::is_same_v<FunctionTraits<decltype(nullary)>::RetT, int>);
+static_assert(std::is_same_v<FunctionTraits<decltype(nullary)>::FuncT,
+                             decltype(nullary)>);
+static_assert(FunctionTraits<decltype(nullary)>::arity == 0);
+
+static_assert(std::is_same_v<FunctionTraits<decltype(unary)>::RetT, int>);
+static_assert(FunctionTraits<decltype(unary)>::arity == 1);
+static_assert(
+    std::is_same_v<FunctionTraits<decltype(unary)>::arg<0>, float>);
+
+static_assert(std::is_same_v<FunctionTraits<decltype(unary_v)>::RetT, void>);
+
+static_assert(FunctionTraits<decltype(binary)>::arity == 2);
+static_assert(
+    std::is_same_v<FunctionTraits<decltype(binary)>::arg<0>, float>);
+static_assert(
+    std::is_same_v<FunctionTraits<decltype(binary)>::arg<1>, double>);
+
+static_assert(FunctionTraits<decltype(&unary)>::arity == 1);
+
+static_assert(FunctionTraits<decltype(unary_lambda)>::arity == 1);
+static_assert(
+    std::is_same_v<FunctionTraits<decltype(unary_lambda)>::RetT, int>);
+static_assert(
+    std::is_same_v<FunctionTraits<decltype(unary_lambda)>::arg<0>,
+                   float>);
+
+static_assert(FunctionTraits<decltype(&Test::nullary)>::arity == 0);
+static_assert(
+    std::is_same_v<FunctionTraits<decltype(&Test::nullary)>::RetT, int>);
+static_assert(
+    std::is_same_v<FunctionTraits<decltype(&Test::nullary)>::ClassT, Test>);
+static_assert(FunctionTraits<decltype(&Test::unary)>::arity == 1);
+static_assert(
+    std::is_same_v<FunctionTraits<decltype(&Test::unary)>::arg<0>, float>);
+static_assert(
+    std::is_same_v<FunctionTraits<decltype(&Test::unary)>::ClassT, Test>);
+
+static_assert(FunctionTraits<NullaryFunctor>::arity == 0);
+static_assert(std::is_same_v<FunctionTraits<NullaryFunctor>::RetT, int>);
+
+static_assert(FunctionTraits<UnaryFunctor>::arity == 1);
+static_assert(
+    std::is_same_v<FunctionTraits<UnaryFunctor>::arg<0>, float>);
+
+static_assert(FunctionTraits<std::function<int(float)>>::arity == 1);
+static_assert(
+    std::is_same_v<FunctionTraits<std::function<int(float)>>::RetT, int>);
+static_assert(
+    std::is_same_v<FunctionTraits<std::function<int(float)>>::arg<0>,
+                   float>);
