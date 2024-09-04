d9400e2d0642a425e69cb9f9feeb0add1baab6ba
Tom Benson <benson31@llnl.gov>
2024-09-04 16:47:56 -0400

    Update copyright years

--- include/h2/meta/typelist/RemoveAll.hpp
+++ include/h2/meta/typelist/RemoveAll.hpp
@@ -1,5 +1,5 @@
 ////////////////////////////////////////////////////////////////////////////////
-// Copyright 2019-2020 Lawrence Livermore National Security, LLC and other
+// Copyright 2019-2024 Lawrence Livermore National Security, LLC and other
 // DiHydrogen Project Developers. See the top-level LICENSE file for details.
 //
 // SPDX-License-Identifier: Apache-2.0
