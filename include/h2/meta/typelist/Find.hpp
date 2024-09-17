95edd8b51f63b0983484442f9ef73bb55885f377
Tom Benson <benson31@llnl.gov>
2024-09-17 18:14:02 -0400

    Cleanup h2::meta::tlist::Find; add h2::meta::tlist::FindIf
    The old version was a bit messy and not tail-recursive. This adds a
    helper template to make things a bit nicer.

--- include/h2/meta/typelist/Find.hpp
+++ include/h2/meta/typelist/Find.hpp
@@ -7,9 +7,8 @@
 
 #pragma once
 
-#include "LispAccessors.hpp"
 #include "TypeList.hpp"
-#include "h2/meta/core/Lazy.hpp"
+#include "h2/meta/core/IfThenElse.hpp"
 #include "h2/meta/core/ValueAsType.hpp"
 
 namespace h2
@@ -18,6 +17,8 @@ namespace meta
 {
 namespace tlist
 {
+constexpr static unsigned long InvalidIdx = static_cast<unsigned long>(-1);
+
 /** @brief Get the index of a given type in the list. */
 template <typename List, typename T>
 struct FindVT;
@@ -32,29 +33,62 @@ constexpr unsigned long FindV()
 template <typename List, typename T>
 inline constexpr unsigned long Find = FindV<List, T>();
 
+/** @brief Get the index of the first type matching Predicate in the list. */
+template <template <typename> class Pred, typename List>
+struct FindIfVT;
+
+/** @brief Get the index of the first type matching Predicate in the list. */
+template <template <typename> class Pred, typename List>
+constexpr unsigned long FindIfV()
+{
+  return FindIfVT<Pred, List>::value;
+}
+
+/** @brief Get the index of the first type matching Predicate in the list. */
+template <template <typename> class Pred, typename List>
+inline constexpr unsigned long FindIf = FindIfV<Pred, List>();
+
 #ifndef DOXYGEN_SHOULD_SKIP_THIS
 
-// Base case
-template <typename T>
-struct FindVT<Empty, T> : ValueAsType<unsigned long, -1UL>
+template <typename List, typename T, unsigned long N>
+struct FindVTImpl;
+
+template <typename T, unsigned long N>
+struct FindVTImpl<Empty, T, N> : ValueAsType<unsigned long, InvalidIdx>
 {};
 
-// Match case
-template <typename... Ts, typename T>
-struct FindVT<TL<T, Ts...>, T> : ValueAsType<unsigned long, 0>
+template <typename... Ts, typename T, unsigned long N>
+struct FindVTImpl<TL<T, Ts...>, T, N> : ValueAsType<unsigned long, N>
 {};
 
-// Recursive case
-template <typename... ListTs, typename T, typename U>
-struct FindVT<TL<T, ListTs...>, U>
-{
-private:
-  static constexpr auto tmp_ = FindV<TL<ListTs...>, U>();
-  static constexpr auto invalid_ = static_cast<unsigned long>(-1);
+template <typename... Ts, typename S, typename T, unsigned long N>
+struct FindVTImpl<TL<S, Ts...>, T, N> : FindVTImpl<TL<Ts...>, T, N + 1>
+{};
 
-public:
-  static constexpr auto value = (tmp_ == invalid_ ? invalid_ : 1UL + tmp_);
-};
+template <typename List, typename T>
+struct FindVT : FindVTImpl<List, T, 0UL>
+{};
+
+template <template <typename> class Pred, typename List, unsigned long N>
+struct FindIfVTImpl;
+
+template <template <typename> class Pred, unsigned long N>
+struct FindIfVTImpl<Pred, Empty, N> : ValueAsType<unsigned long, InvalidIdx>
+{};
+
+template <template <typename> class Pred,
+          typename T,
+          typename... Ts,
+          unsigned long N>
+struct FindIfVTImpl<Pred, TL<T, Ts...>, N>
+  : IfThenElse<Pred<T>::value,
+               ValueAsType<unsigned long, N>,
+               FindIfVTImpl<Pred, TL<Ts...>, N + 1>>
+{};
+
+template <template <typename> class Pred, typename List>
+struct FindIfVT : FindIfVTImpl<Pred, List, 0UL>
+{};
 
 #endif  // DOXYGEN_SHOULD_SKIP_THIS
 }  // namespace tlist
