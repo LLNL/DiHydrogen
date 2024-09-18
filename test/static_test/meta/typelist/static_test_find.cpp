18869a72e2fcff657d18eb94511949abc09d935b
Tom Benson <benson31@llnl.gov>
2024-09-17 21:28:22 -0400

    Cleanup h2::meta::tlist::Find; add h2::meta::tlist::FindIf (#187)
    The old version was a bit messy and not tail-recursive. This adds a
    helper template to make things a bit nicer.

--- test/static_test/meta/typelist/static_test_find.cpp
+++ test/static_test/meta/typelist/static_test_find.cpp
@@ -5,19 +5,37 @@
 // SPDX-License-Identifier: Apache-2.0
 ////////////////////////////////////////////////////////////////////////////////
 
-#include "h2/meta/Core.hpp"
+#include "h2/meta/core/Eq.hpp"
 #include "h2/meta/typelist/Find.hpp"
 
 using namespace h2::meta;
 using namespace h2::meta::tlist;
 
-using TList = TL<int, float, bool>;
+using TList = TL<char, signed char, short, int, long, long long>;
 
 // Testing Find
-static_assert(FindV<TList, int>() == 0UL, "Find int in list");
-static_assert(FindV<TList, bool>() == 2UL, "Find bool in list");
-static_assert(FindV<TL<int, TList, bool>, TList>() == 1UL, "Find TL in list");
-static_assert(FindV<Empty, bool>() == static_cast<unsigned long>(-1),
-              "Find in an empty list.");
-static_assert(FindV<TL<int>, bool>() == static_cast<unsigned long>(-1),
+static_assert(Find<Empty, int> == InvalidIdx, "Find in an empty list.");
+
+static_assert(Find<TList, char> == 0UL, "Find char in list.");
+static_assert(Find<TList, int> == 3UL, "Find int in list.");
+static_assert(FindV<TList, int>() == 3UL,
+              "Find int in list, function version.");
+static_assert(Find<TList, long long> == 5UL, "Find long long in list.");
+
+static_assert(Find<TList, float> == InvalidIdx,
               "Find nonexistent type in list.");
+
+// Find returns the first match's index
+static_assert(Find<TL<int, double, char, double>, double> == 1UL,
+              "Find returns first matching index.");
+
+// Testing FindIf
+template <typename T>
+using IsInt = EqVT<int, T>;
+
+template <typename T>
+using IsDbl = EqVT<double, T>;
+
+static_assert(FindIf<IsInt, TList> == 3UL, "Find first int in list.");
+static_assert(FindIf<IsDbl, TList> == InvalidIdx,
+              "FindIf returns InvalidIdx if no matching type found.");
